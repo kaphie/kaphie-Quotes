c93a6e2733a110cf7ce482e9aa73701aa5d1b849
kaphie <bkafrika144@gmail.com>
2020-03-31 14:11:17 +0300

    adding ts in sidebar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
+import {Quote} from '../quote';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  @Input () quote: Quote;
 
-  ngOnInit(): void {
+
+  constructor() {
+  }
+
+  ngOnInit() {
   }
 
 }

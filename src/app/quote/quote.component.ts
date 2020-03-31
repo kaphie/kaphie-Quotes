811bdfa1365f7be2d6c7e0ea587d4d25c0402751
kaphie <bkafrika144@gmail.com>
2020-03-31 14:21:03 +0300

    making corrections

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -22,14 +22,14 @@ export class QuoteComponent implements OnInit {
   @Output() myData = new EventEmitter();
   quotes: Quote[] = [
     // tslint:disable-next-line:max-line-length
-    new Quote('Love', 'It was love at first sight, at last sight, at ever and ever sight.', 'Vladimir Nabokov', new Date(2019, 8 - 1, 17), 0, 0),
-    new Quote('Health ', 'To enjoy the glow of good health, you must exercise.', 'Gene Tunney', new Date(2019, 8 - 1, 17), 0, 0),
-    new Quote('Tech ', 'Technology is best when it brings people together.', 'Matt Mullenweg', new Date(2019, 8 - 1, 18), 0, 0),
-    new Quote('Life', 'Simplicity is the ultimate sophistication.', 'Leonardo da Vinci', new Date(2019, 8 - 1, 18), 0, 0),
+    new Quote('Love', 'It was love at first sight, at last sight, at ever and ever sight.', 'pip Bbaale', new Date(2019, 8 - 1, 17), 0, 0),
+    new Quote('Health ', 'To enjoy the glow of good health, you must exercise.', 'Lenny Johns', new Date(2019, 8 - 1, 17), 0, 0),
+    new Quote('Tech ', 'Technology is best when it brings people together.', 'Matt conrad', new Date(2019, 8 - 1, 18), 0, 0),
+    new Quote('Life', 'Simplicity is the ultimate sophistication.', 'Ashla Lattif', new Date(2019, 8 - 1, 18), 0, 0),
     // tslint:disable-next-line:max-line-length
-    new Quote('Humour ', 'If you could choose one characteristic that would get you through life, choose a sense of humor.', 'Jennifer Jones', new Date(2019, 8 - 1, 17), 0, 0),
+    new Quote('Humour ', 'If you could choose one characteristic that would get you through life, choose a sense of humor.', 'Maya Anomlous', new Date(2019, 8 - 1, 17), 0, 0),
     // tslint:disable-next-line:max-line-length
-    new Quote('Wisdom ', 'It is the mark of an educated mind to be able to entertain a thought without accepting it.', 'Aristotle', new Date(2019, 8 - 1, 17), 0, 0)
+    new Quote('Wisdom ', 'It is the mark of an educated mind to be able to entertain a thought without accepting it.', 'Keisha Jill Martha', new Date(2019, 8 - 1, 17), 0, 0)
   ];
 
   public upvote: true;
